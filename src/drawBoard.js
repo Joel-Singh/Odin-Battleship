f7a99195203e34726e2526eb6b45f029b7d0310f
Joel Singh <joelsingh788@gmail.com>
2022-11-18 16:58:08 -0500

    Refactor to have board creation coupled with cells

--- src/drawBoard.js
+++ src/drawBoard.js
@@ -1,8 +1,6 @@
 
 function drawBoard(x, y) {
-  let board = document.createElement('div')
-  board.classList.add('board')
-  add100CellsTo(board)
+  let board = createBoardWithCells()
 
   if (x !== undefined) {
     getCell(x, y, board).classList.add('hit')
@@ -11,18 +9,31 @@ function drawBoard(x, y) {
   return board
 }
 
-function add100CellsTo(board) {
-  const NUM_OF_CELLS = 100
-  for (let i = 0; i < NUM_OF_CELLS; i++) {
-    let newCell = createCell()
-    board.append(newCell)
+function createBoardWithCells() {
+  let board = createBoard()
+  add100CellsTo(board)
+  return board
+
+  function createBoard() {
+    let board = document.createElement('div')
+    board.classList.add('board')
+    return board
   }
 
-  function createCell() {
-    let newCell = document.createElement('div')
-    newCell.classList.add('cell')
-    return newCell
+  function add100CellsTo(board) {
+    const NUM_OF_CELLS = 100
+    for (let i = 0; i < NUM_OF_CELLS; i++) {
+      let newCell = createCell()
+      board.append(newCell)
+    }
+
+    function createCell() {
+      let newCell = document.createElement('div')
+      newCell.classList.add('cell')
+      return newCell
+    }
   }
+
 }
 
 // One based index. origin is bottom left
