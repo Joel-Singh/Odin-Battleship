5803c7e4d40c3261bd8858ded2248daad17e54f8
Joel Singh <joelsingh788@gmail.com>
2022-11-20 10:06:22 -0500

    Format

--- src/point.js
+++ src/point.js
@@ -1,23 +1,25 @@
 export function Point(x, y) {
   function getX() {
-    return x
+    return x;
   }
   function getY() {
-    return y
+    return y;
   }
-  return {getX, getY}
+  return { getX, getY };
 }
 
 export function PointList() {
-  let pointArr = []
+  let pointArr = [];
   function add(x, y) {
-    let newPoint = Point(x, y)
-    pointArr.push(newPoint)
+    let newPoint = Point(x, y);
+    pointArr.push(newPoint);
   }
   function has(x, y) {
-    return pointArr.findIndex((point) =>
-      point.getX() === x && point.getY() === y
-    ) !== -1
+    return (
+      pointArr.findIndex(
+        (point) => point.getX() === x && point.getY() === y
+      ) !== -1
+    );
   }
-  return {add, has}
+  return { add, has };
 }
