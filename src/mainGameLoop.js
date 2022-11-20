359ca617e5ce78324dda44d1d8d4113f9bdff129
Joel Singh <joelsingh788@gmail.com>
2022-11-20 12:55:54 -0500

    Add intializeDOM function

---
+++ src/mainGameLoop.js
@@ -0,0 +1,51 @@
+import { drawBoard } from "./drawBoard"
+
+function initializeDOMWithTwoGameboards() {
+  let body = document.querySelector('body')
+  let gameBoards = createTwoGameBoards()
+  body.append(gameBoards)
+
+
+  function createTwoGameBoards() {
+    let playerSide = createSide('Player', 'player-side')
+    let enemySide = createSide('Enemy', 'enemy-side')
+    let gameWrapper = createGameWrapper()
+    gameWrapper.append(playerSide, enemySide)
+    return gameWrapper
+
+    function createSide(title, className) {
+      let sideWrapper = createSideWrapper()
+      let sideTitle = createSideTitle()
+      let board = createBoard()
+
+      sideWrapper.append(sideTitle, board)
+
+      return sideWrapper
+
+      function createSideWrapper() {
+        let sideWrapper = document.createElement('span')
+        sideWrapper.classList.add('side-wrapper')
+        sideWrapper.classList.add(className)
+        return sideWrapper
+      }
+
+      function createSideTitle() {
+        let sideTitle = document.createElement('div')
+        sideTitle.classList.add('side-title')
+        sideTitle.textContent = title
+        return sideTitle
+      }
+
+      function createBoard() {
+        let board = drawBoard()
+        return board
+      }
+    }
+
+    function createGameWrapper() {
+      let gameWrapper = document.createElement('div')
+      gameWrapper.classList.add('game-wrapper')
+      return gameWrapper
+    }
+  }
+}
