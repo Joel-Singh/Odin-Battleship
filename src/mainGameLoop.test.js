93dfe9b2d05c35c160c07dc36966ebe98f625656
Joel Singh <joelsingh788@gmail.com>
2022-11-21 12:59:27 -0500

    Rename board to domBoard

--- src/mainGameLoop.test.js
+++ src/mainGameLoop.test.js
@@ -5,36 +5,36 @@
 import { drawBoard } from "./drawBoard";
 import { allowOneCellToBeHit } from './mainGameLoop'
 
-let board;
+let domBoard;
 beforeEach(() => {
-  board = drawBoard()
+  domBoard = drawBoard()
 })
 
 test('Hit class added to (3, 4) cell after adding event listener and clicking it', () => {
-  allowOneCellToBeHit(board)
-  let threeFourCell = board.querySelector('[data-x="3"][data-y="4"]')
+  allowOneCellToBeHit(domBoard)
+  let threeFourCell = domBoard.querySelector('[data-x="3"][data-y="4"]')
   threeFourCell.click()
   let isHit = threeFourCell.classList.contains('hit')
   expect(isHit).toBe(true)
 })
 
 test('Hit class added ONLY to (2, 8) cell after adding event listener and clicking it', () => {
-  allowOneCellToBeHit(board)
-  let twoEightCell = board.querySelector('[data-x="2"][data-y="8"]')
+  allowOneCellToBeHit(domBoard)
+  let twoEightCell = domBoard.querySelector('[data-x="2"][data-y="8"]')
   twoEightCell.click()
   let isTwoEightHit = twoEightCell.classList.contains('hit')
   expect(isTwoEightHit).toBe(true)
 
-  let sixSevenCell = board.querySelector('[data-x="6"][data-y="7"]')
+  let sixSevenCell = domBoard.querySelector('[data-x="6"][data-y="7"]')
   let isSixSevenHit = sixSevenCell.classList.contains('hit')
   expect(isSixSevenHit).toBe(false)
 })
 
 test('Hit class added ONLY to (3, 4) cell after clicking two cells', () => {
-  allowOneCellToBeHit(board)
+  allowOneCellToBeHit(domBoard)
 
-  let threeFourCell = board.querySelector('[data-x="3"][data-y="4"]')
-  let nineTwoCell = board.querySelector('[data-x="9"][data-y="2"]')
+  let threeFourCell = domBoard.querySelector('[data-x="3"][data-y="4"]')
+  let nineTwoCell = domBoard.querySelector('[data-x="9"][data-y="2"]')
 
   threeFourCell.click()
   nineTwoCell.click()
