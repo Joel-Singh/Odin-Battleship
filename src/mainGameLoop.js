8733b9e823a159bd3ecab1c22ea82d2ef7ba5bbf
Joel Singh <joelsingh788@gmail.com>
2022-11-21 14:43:52 -0500

    format

--- src/mainGameLoop.js
+++ src/mainGameLoop.js
@@ -1,41 +1,32 @@
 function allowOneCellToBeHit(DOMBoard, ObjBoard) {
-  let allCells = [...DOMBoard.querySelectorAll('.cell')]
-  addHitFunctionToAll()
+  let allCells = [...DOMBoard.querySelectorAll(".cell")];
+  addHitFunctionToAll();
 
   let cellHasBeenHitResolvePromise;
   return new Promise((resolve) => {
-    cellHasBeenHitResolvePromise = resolve
-  })
+    cellHasBeenHitResolvePromise = resolve;
+  });
 
   function hitFunction(e) {
-    e.target.classList.add('hit')
-    updateObjBoard()
-    removeHitFunctionFromAll()
-    cellHasBeenHitResolvePromise()
+    e.target.classList.add("hit");
+    updateObjBoard();
+    removeHitFunctionFromAll();
+    cellHasBeenHitResolvePromise();
 
     function updateObjBoard() {
-      let x = Number.parseInt(e.target.getAttribute('data-x'))
-      let y = Number.parseInt(e.target.getAttribute('data-y'))
-      ObjBoard.hit(x, y)
+      let x = Number.parseInt(e.target.getAttribute("data-x"));
+      let y = Number.parseInt(e.target.getAttribute("data-y"));
+      ObjBoard.hit(x, y);
     }
   }
 
   function addHitFunctionToAll() {
-    allCells.forEach(
-      cell =>
-        cell.addEventListener('click', hitFunction)
-    )
+    allCells.forEach((cell) => cell.addEventListener("click", hitFunction));
   }
 
   function removeHitFunctionFromAll() {
-    allCells.forEach(
-      cell =>
-        cell.removeEventListener('click', hitFunction)
-    )
+    allCells.forEach((cell) => cell.removeEventListener("click", hitFunction));
   }
-
-
 }
 
-
-export { allowOneCellToBeHit }
+export { allowOneCellToBeHit };
